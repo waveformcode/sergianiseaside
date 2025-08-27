1931d1b4931c2bec4418d41b367b7c87d9edb17b
waveformcode <maniadakis.chr@gmail.com>
2025-08-27 17:02:39 +0300

    dropdown + js

---
+++ script.js
@@ -0,0 +1,18 @@
+const paragraphs = document.querySelectorAll(".fade-in");
+
+document.addEventListener("scroll", function () {
+  paragraphs.forEach((paragraph) => {
+    if (isInView(paragraph)) {
+      paragraph.classList.add("fade-in--visible");
+    }
+  });
+});
+
+function isInView(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.top <
+      (window.innerHeight - 150 || document.documentElement.clientHeight - 150)
+  );
+}
